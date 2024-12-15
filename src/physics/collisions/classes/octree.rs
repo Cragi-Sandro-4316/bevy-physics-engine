d91396b03037656f606aae5d8b5be3cf35da1364
Cragi-Sandro4316 <filippomacciardi4@gmail.com>
2024-12-15 21:25:45 +0100

    implemented insert and split methods to struct OctreeNode

--- src/physics/collisions/classes/octree.rs
+++ src/physics/collisions/classes/octree.rs
@@ -1,5 +1,7 @@
-use bevy::prelude::Entity;
-use parry3d::bounding_volume::Aabb;
+use bevy::prelude::*;
+use parry3d::{bounding_volume::{Aabb, BoundingVolume}, shape::Shape};
+
+use super::{subdivide_aabb, transform_to_isometry};
 
 const MAX_ENTITIES: u8 = 16; 
 
@@ -7,5 +9,78 @@ const MAX_ENTITIES: u8 = 16;
 pub struct OctreeNode {
     children: Option<[Box<OctreeNode>; 8]>, // None if this is a leaf node
     bounding_box: Aabb,
-    objects: Vec<Entity>
+    objects: Vec<PhysicsEntity>
+}
+
+pub struct PhysicsEntity {
+    pub entity: Entity,                     // the bevy entity
+    pub collider: Box<dyn Shape>,    // the collider shape
+    pub transform: Transform,        // the bevy transform
+}
+
+impl OctreeNode {
+    fn insert(
+        &mut self, 
+        physics_entity: PhysicsEntity,   // the PhysicsEntity to insert
+        max_objects: usize,             // the max number of objects in a node
+        max_depth: usize                // the max depth of a node
+    ) {
+        let entity_isometry = transform_to_isometry(&physics_entity.transform);
+        
+        // if the node has children try inserting into one of them
+        if let Some(children) = &mut self.children {
+            for child in children.iter_mut() {
+                if child.bounding_box.contains(
+                    &physics_entity.collider.compute_aabb(&entity_isometry)
+                ) {
+                    child.insert(physics_entity, max_objects, max_depth - 1);
+                    return;
+                }
+            }
+        }
+
+        // otherwise, store the object in this node
+        self.objects.push(physics_entity);
+
+        // check if the node should be split
+        if self.objects.len() > max_objects && max_depth > 0 {
+            self.split(max_objects, max_depth);
+        }
+
+    }
+
+
+
+    fn split(&mut self, max_objects: usize, max_depth: usize) {
+        // Subdivide the current bounding box into eight smaller regions
+        let sub_boxes = subdivide_aabb(self.bounding_box);
+
+        // Create eight child nodes, each with one of the new bounding boxes
+        self.children = Some(sub_boxes.map(|bbox| Box::new(OctreeNode {
+            bounding_box: bbox,
+            objects: Vec::new(),
+            children: None,
+        })));
+
+        // Temporarily take the objects stored in this node
+        let entities = std::mem::take(&mut self.objects);
+
+        // Redistribute the objects into the appropriate child nodes
+        for physics_entity in entities {
+            // converts the bevy Transform to a parry3d Isometry for this entity
+            let entity_isometry = transform_to_isometry(&physics_entity.transform);
+
+            for child in self.children.as_mut().unwrap().iter_mut() {
+                if child.bounding_box.contains(
+                    &physics_entity.collider.compute_aabb(&entity_isometry)
+                ) {
+                    child.insert(physics_entity, max_objects, max_depth - 1);
+                    break; // an object should only go in one child
+                }
+            }
+
+        }
+
+    }
+
 }
--- src/physics/collisions/utils/utils.rs
+++ src/physics/collisions/utils/utils.rs
@@ -1,5 +1,5 @@
-use nalgebra::{Isometry, Isometry3, Quaternion, Unit};
-use parry3d::shape::SharedShape;
+use nalgebra::{Isometry, Isometry3, Point3, Quaternion, Unit};
+use parry3d::{bounding_volume::Aabb, shape::SharedShape};
 use bevy::{prelude::*, render::mesh::{Indices, VertexAttributeValues}};
 
 
@@ -43,3 +43,56 @@ pub fn extract_mesh_vertices_indices(mesh: &Mesh) -> Option<VerticesIndices> {
 }
 
 
+pub fn subdivide_aabb(
+    bounding_box: Aabb
+) -> [Aabb; 8] {
+    
+    let min = bounding_box.mins;
+    let max = bounding_box.maxs;
+    let center = bounding_box.center();
+
+    [
+        // Bottom-left-front
+        Aabb::new(min, center),
+        // Bottom-right-front
+        Aabb::new(Point3::new(center.x, min.y, min.z), Point3::new(max.x, center.y, center.z)),
+        // Bottom-left-back
+        Aabb::new(Point3::new(min.x, min.y, center.z), Point3::new(center.x, center.y, max.z)),
+        // Bottom-right-back
+        Aabb::new(Point3::new(center.x, min.y, center.z), Point3::new(max.x, center.y, max.z)),
+        // Top-left-front
+        Aabb::new(Point3::new(min.x, center.y, min.z), Point3::new(center.x, max.y, center.z)),
+        // Top-right-front
+        Aabb::new(Point3::new(center.x, center.y, min.z), Point3::new(max.x, max.y, center.z)),
+        // Top-left-back
+        Aabb::new(Point3::new(min.x, center.y, center.z), Point3::new(center.x, max.y, max.z)),
+        // Top-right-back
+        Aabb::new(center, max),
+    ]
+}
+
+
+// converts a bevy transform into a parry Isometry for collision detection
+pub fn transform_to_isometry(transform: &Transform) -> Isometry<f32, Unit<Quaternion<f32>>, 3> {
+    let mut isometry3d = Isometry3::translation(
+        transform.translation.x, 
+        transform.translation.y, 
+        transform.translation.z
+    );
+
+    let collider_transform = transform.clone();
+
+
+    let coll_rotation = collider_transform.rotation;
+
+    isometry3d.rotation = Unit::new_normalize(Quaternion::new(
+        coll_rotation.w,
+        coll_rotation.x, 
+
+        coll_rotation.y, 
+        coll_rotation.z,     
+    ));
+
+
+    isometry3d
+}
